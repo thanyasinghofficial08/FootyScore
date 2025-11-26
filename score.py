73896f6b586c4b160fe3f4671c8ab78ecebb182d
thanyasinghofficial08 <thanyasinghofficial08@gmail.com>
2025-11-26 14:17:49 +0530

    Add first football score script

---
+++ score.py
@@ -0,0 +1,9 @@
+match={
+    "team1": "Barcelona",
+    "team2": "Real Madrid",
+    "score": "2 - 1"
+}
+
+print("Match Score:")
+print(match["team1"], "vs", match["team2"])
+print("Final Score:", match["score"])
